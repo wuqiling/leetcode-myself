2fc70d4fe49b603a698ef3e693c173c8c075d310
wuqiling <wuqiling28@gmail.com>
2018-10-31 11:22:57 +0800

    做了几道

---
+++ leetcode_myself/055.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <assert.h>
+using std::cout;
+using std::endl;
+
+
+void printArr(const int *num, int len) {
+	assert(num != NULL&&len >= 0);
+	for (int i = 0; i < len; i++)
+		cout << i << " :" << num[i] << ", ";
+	cout << ",len:" << len << endl;
+}
+
+bool canJump(const int *nums, int numsSize)
+{
+	printArr(nums, numsSize);
+	if (nums == NULL || numsSize <= 0)
+		return false;
+	else if (numsSize == 1 || nums[0] >= numsSize - 1)
+		return true;
+	else if (numsSize == 2)
+	{
+		if (nums[0] > 0)
+			return true;
+		else
+			return false;
+	}
+	for (int i = numsSize - 3; i >= 0; i--)
+	{
+		cout << "i:" << i << " num i:" << nums[i] << " num i+1:" << nums[i + 1] << " jump :" << numsSize - i - 2 << endl;
+		if (nums[i] > nums[i + 1])
+		{
+			continue;
+		}
+		else if (nums[i + 1] >= numsSize - i - 2)
+		{
+			return canJump(nums, i + 2);
+		}
+	}
+	return false;
+}
+
+int main()
+{
+	int arr[] = {0, 0, 4 };
+	int len = sizeof(arr) / sizeof(arr[0]);
+	if (canJump(arr, len))
+		cout << "\n T rue";
+	else
+		cout << "\n F alse";
+	return 0;
+}
---
+++ leetcode_myself/069.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int mySqrt(int x)
+{
+	if (x == 0)
+		return 0;
+	else if (x == 1)
+		return 1;
+	long long i = 0;
+	long long j = x / 2 + 1;
+	while (i < j)
+	{
+		long long mid = (i + j) / 2;
+		long long sq = mid * mid;
+		if (sq < x)
+			i = mid;
+		else if (sq > x)
+			j = mid;
+		else
+			return mid;
+		if (i + 1 == j)
+			return i;
+	}
+	return j;
+}
+
+int main()
+{
+	int b = 4;
+	int a = mySqrt(b);
+	cout << a << endl;
+	return 0;
+}
