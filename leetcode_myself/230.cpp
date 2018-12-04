3e3489349a37f5320defdb0acb97e8618fb104df
wuqiling <wuqiling28@gmail.com>
2018-12-04 11:00:44 +0800

    add 230

---
+++ leetcode_myself/230.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution {
+   public:
+    // time:O(N) space:O(N)	runtime 16ms 33%
+    int kthSmallest(TreeNode *root, int k) {
+        globalK = k;
+        res = 0;
+        dfs(root);
+        return res;
+    }
+
+   private:
+    // dfs
+    //In-order traversal(Preorder traversal Post-order traversal)
+    void dfs(TreeNode *root) {
+        if (root == NULL) return;
+        dfs(root->left);
+        globalK--;
+        if (globalK == 0) res = root->val;
+        dfs(root->right);
+    }
+
+   private:
+    int globalK = 0;
+    int res;
+};
